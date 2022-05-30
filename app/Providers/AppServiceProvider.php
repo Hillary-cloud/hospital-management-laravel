285b7bf2ae7af25505f6c4082da7ce933234dcff
hillary-cloud <obicj97@gmail.com>
2022-05-30 08:00:18 -0700

    made change in provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        Schema::defaultSringLenght(191);
+        Schema::defaultSringLength(191);
     }
 
     /**
